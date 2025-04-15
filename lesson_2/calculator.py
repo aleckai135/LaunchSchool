0a82032037e484d6f5406a0a4115f0992a66591b
Alec S <alecsacry@yahoo.com>
2025-04-15 00:30:15 +0000

    .

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,32 +1,60 @@
 print('Welcome to Calculator!')
 
+# function that includes unique intro
+
+def prompt(s):
+    print('==> ', s)
+
+# function that catches non-int
+
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
 # Ask the user for the first number.
 
-print('Enter first number:')
+prompt('Enter first number:')
 number1 = input()
+
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
 
 # Ask the user for the second number.
 
-print('Enter second number:')
+prompt('Enter second number:')
 number2 = input()
+
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
 
 # Ask the user for an operation to perform.
 
-print('Enter desired operation:\n' \
+prompt('Enter desired operation:\n' \
 '1. add 2. subtract 3. multiply 4. divide')
 operation = input()
 
+while operation not in ["1", "2", "3", "4"]:
+    prompt('You must choose 1, 2, 3, or 4')
+    operation = input()
+
 # Perform the operation on the two numbers.
 
-if operation == '1':
-  output = int(number1) + int(number2)
-elif operation == '2':
-  output = int(number1) - int(number2)
-elif operation == '3':
-  output = int(number1) * int(number2)
-elif operation == '4':
-  output = int(number1) / int(number2)
+match operation:
+    case '1':
+        output = int(number1) + int(number2)
+    case '2':
+        output = int(number1) - int(number2)
+    case '3':
+        output= int(number1) * int(number2)
+    case '4':
+        output = int(number1) / int(number2)
 
 # Print the result to the terminal.
 
-print(f'The result is: {output}')
+prompt(f'The result is: {output}')
