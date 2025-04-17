d54bc9dcbec9de67c149fa0f811d0247d9a6db24
Alec S <alecsacry@yahoo.com>
2025-04-17 21:33:47 +0000

    .

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,3 +1,6 @@
+import pdb # for debugging purposes - pdb.set_trace()
+import json # for storing messages in config file
+
 # function for cool prompt text
 def prompt(message):
     print(f"==> {message}")
@@ -5,52 +8,70 @@ def prompt(message):
 # function for invalid number handling
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
 
     return False
 
+# json config file for storing messages
+with open('comments.json', 'r') as file:
+    COMMENTS = json.load(file)
+
+# ask for language preference
+print("For english - en | For spanish - es")
+LANG = input()
+
 # print greeting
-prompt('Welcome to Calculator!')
+prompt(COMMENTS[LANG]['welcome'])
 
-# prompt for first number
-prompt("What's the first number?")
-number1 = input()
+# another operation loop start
+while True:
 
-# loop for invalid number
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
+    # prompt for first number
+    prompt("What's the first number?")
     number1 = input()
 
-# prompt for first number
-prompt("What's the second number?")
-number2 = input()
+    # loop for invalid number
+    while invalid_number(number1):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number1 = input()
 
-# loop for invalid number
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
+    # prompt for first number
+    prompt("What's the second number?")
     number2 = input()
 
-# prompt for operation
-prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
-operation = input()
+    # loop for invalid number
+    while invalid_number(number2):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number2 = input()
 
-# loop for invalid operation
-while operation not in ["1", "2", "3", "4"]:
-    prompt("You must choose 1, 2, 3, or 4")
+    # prompt for operation
+    prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
     operation = input()
 
-# match/case for operation choice
-match operation:
-    case "1":
-        output = int(number1) + int(number2)
-    case "2":
-        output = int(number1) - int(number2)
-    case "3":
-        output = int(number1) * int(number2)
-    case "4":
-        output = int(number1) / int(number2)
+    # loop for invalid operation
+    while operation not in ["1", "2", "3", "4"]:
+        prompt("You must choose 1, 2, 3, or 4")
+        operation = input()
 
-# print result
-prompt(f"The result is {output}")
+    # match/case for operation choice
+    match operation:
+        case "1":
+            output = float(number1) + float(number2)
+        case "2":
+            output = float(number1) - float(number2)
+        case "3":
+            output = float(number1) * float(number2)
+        case "4":
+            output = float(number1) / float(number2)
+
+    # print result
+    prompt(f"The result is {output}")
+
+    # ask user if go again
+    prompt('Would you like to do another operation? (y) ')
+    again = input()
+
+    if again != 'y':
+        break
